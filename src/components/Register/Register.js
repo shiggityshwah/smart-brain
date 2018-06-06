466527cb3c4e1ee7442e4bdde4c3675fdfd12563
Stefano M <stefanom@gmail.com>
2018-06-06 01:24:57 -0700

    added backend

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
     }
 
     onSubmitRegister = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://protected-cove-48569.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
